4eee2acff693607e372d56e99d0586b3f7669a32
Wojciech Andreas Jurczyk <wajurczyk@gmail.com>
2012-11-11 21:48:13 +0100

    added ping handling to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var io = require("socket.io");
 var server = io.listen(1337).set("log level", 1);
 
+var matches = [];
+
 server.sockets.on("connection", function(socket) {
 	socket.on("msg", function(data){
 		server.sockets.emit("broadcast", "serverMSG");
@@ -9,5 +11,10 @@ server.sockets.on("connection", function(socket) {
 	});
 	socket.on("createMatch", function(data) {
 		console.log("create match: "+ data);
+	});
+
+	socket.on("ping", function( data ) {	
+		console.log("PING with data: " + data);
+		socket.emit("pingBack", matches.length);
 	});
 });
